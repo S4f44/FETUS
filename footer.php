f1afbf0fa420bd2a3ffeb48152f12a9e4ebc363c
S4f44 <bencersafa8@gmail.com>
2023-12-12 17:01:24 +0100

    footer

--- footer.php
+++ footer.php
@@ -1,4 +1,48 @@
 <?php wp_footer(); ?>
+
+<!------------------FOOTER----------------->
+  
+<footer class="footer">
+        <div class="container">
+          <div class="row">
+            <div class="footer-col">
+              <a class="d-inline-flex align-items-center mb-2 text-body-emphasis" href="/" >
+                <img class="logo" src="assets/img/logo.svg" alt="logo FetUs">
+              </a>
+            </div>
+
+            <div class="footer-col">
+              <h4>Général</h4>
+              <ul>
+                <li><a href="#">Tout savoir sur la grossesse</a></li>
+                <li><a href="#">Conseils</a></li>
+                <li><a href="#">Mon suivi</a></li>
+                <li><a href="#">Mes objectifs</a></li>
+              </ul>
+            </div>
+            <div class="footer-col">
+              <h4>Outils</h4>
+              <ul>
+                <li><a href="#">Mentions légales</a></li>
+                <li><a href="#">Cookies</a></li>
+                <li><a href="#">Se connecter</a></li>
+                <li><a href="#">S'enregistrer</a></li>
+              </ul>
+            </div>
+            <div class="footer-col">
+              <h4>Suivez-nous</h4>
+              <div class="social-links">
+                <a href="#"><img src="assets/img/x.svg" alt="X"></a>
+                <a href="#"><img src="assets/img/youtube.svg" alt="youtube"></i></a>
+                <a href="#"><img src="assets/img/instagram.svg" alt="instagram"></a>
+                <a href="#"><img src="assets/img/facebook.svg" alt="facebook"></a>
+              </div>
+            </div>
+          </div>
+        </div>
+     </footer>
+
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
 <script src="<?php echo get_template_directory_uri() ?>/assets/js/monjs.js"></script>
--- index.php
+++ index.php
@@ -213,48 +213,7 @@
     </main>
 
 
-<!------------------FOOTER----------------->
-  
-      <footer class="footer">
-        <div class="container">
-          <div class="row">
-            <div class="footer-col">
-              <a class="d-inline-flex align-items-center mb-2 text-body-emphasis" href="/" >
-                <img class="logo" src="assets/img/logo.svg" alt="logo FetUs">
-              </a>
-            </div>
 
-            <div class="footer-col">
-              <h4>Général</h4>
-              <ul>
-                <li><a href="#">Tout savoir sur la grossesse</a></li>
-                <li><a href="#">Conseils</a></li>
-                <li><a href="#">Mon suivi</a></li>
-                <li><a href="#">Mes objectifs</a></li>
-              </ul>
-            </div>
-            <div class="footer-col">
-              <h4>Outils</h4>
-              <ul>
-                <li><a href="#">Mentions légales</a></li>
-                <li><a href="#">Cookies</a></li>
-                <li><a href="#">Se connecter</a></li>
-                <li><a href="#">S'enregistrer</a></li>
-              </ul>
-            </div>
-            <div class="footer-col">
-              <h4>Suivez-nous</h4>
-              <div class="social-links">
-                <a href="#"><img src="assets/img/x.svg" alt="X"></a>
-                <a href="#"><img src="assets/img/youtube.svg" alt="youtube"></i></a>
-                <a href="#"><img src="assets/img/instagram.svg" alt="instagram"></a>
-                <a href="#"><img src="assets/img/facebook.svg" alt="facebook"></a>
-              </div>
-            </div>
-          </div>
-        </div>
-     </footer>
-   
   
 
 <?php get_footer(); ?>
