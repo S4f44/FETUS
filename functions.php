3498b87ad78133b0d0b4d0af741def9b46499c24
S4f44 <bencersafa8@gmail.com>
2023-12-13 00:59:48 +0100

    quoicoubeh

--- functions.php
+++ functions.php
@@ -7,7 +7,7 @@ register_nav_menu('header', 'En tête du menu');
 
 function wpbootstrap_styles_scripts() {
   wp_enqueue_style('style', get_stylesheet_uri());
-  wp_enqueue_style('bootstrap', '	https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css');
-  wp_enqueue_script('bootstrap-bundle', '	https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js', false, '1.0.0', true);
+  wp_enqueue_style('bootstrap', '	https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css');
+  wp_enqueue_script('bootstrap-bundle', '	https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js', false, '1.0.0', true);
 }
 add_action('wp_enqueue_scripts', 'wpbootstrap_styles_scripts');
