bd21f7a8ba557c447421bb5aef09fd1a15ea716f
Nell Gaspar <nell.gaspar09@gmail.com>
2023-12-13 03:00:27 +0100

    carousel debut 2

--- header.php
+++ header.php
@@ -29,7 +29,7 @@
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <?php wp_nav_menu(['theme_location' => 'header']); ?>
-      <!-- <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="#">Home</a>
         </li>
