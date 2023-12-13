c37e00343898d2bdfee740661bae629c0b2cfdef
S4f44 <bencersafa8@gmail.com>
2023-12-13 15:48:53 +0100

    index réglages

--- index.php
+++ index.php
@@ -29,7 +29,7 @@
         </div>
       </section>
       
-    <section class="fruits">
+    <!-- <section class="fruits">
         <div class="container text-center">
           <div class="row">
             <div class="col-md-10 offset-md-1 my-5">
@@ -47,13 +47,13 @@
             </div>
           </div>
         </div>
-      </section>
+      </section> -->
 
       
-<!-- <section class="fruits">
+<section class="fruits">
 
 <div id="fruitCarousel" class="carousel slide" data-ride="carousel">
-    <div class="carousel-inner">-->
+    <div class="carousel-inner">
         <?php
         $fruits = array(
             array(
@@ -159,175 +159,8 @@
                 </div>
               </a>
             </div>
-
-
-
- <!-- enlever animation  automatique des boutons  -->
-<!-- <script>
-    jQuery(document).ready(function ($) {
-        // Désactiver l'animation automatique
-        $('#fruitCarousel').carousel({
-            interval: false
-        });
-
-        // Gérer le clic sur le bouton "Previous"
-        $('#fruitCarousel').on('slide.bs.carousel', function (e) {
-            if (e.direction === 'left') {
-                // Animation vers la gauche (Previous)
-                $('#fruitCarousel').carousel('prev');
-            }
-        });
-
-        // Gérer le clic sur le bouton "Next"
-        $('#fruitCarousel').on('slide.bs.carousel', function (e) {
-            if (e.direction === 'right') {
-                // Animation vers la droite (Next)
-                $('#fruitCarousel').carousel('next');
-            }
-        });
-    });
-</script> 
-
-
-</section> -->
-
-
-
-
-
-<!-- 
-    <section class="fruits">
-
-    <section class="fruits">
-      <div class="container py-3">
-        <div class="row">
-          <div class="col-md mb-3 d-flex">
-            <div class="images js-images">        
-              <img data-index="step-1" class="active" src=" //echo get_template_directory_uri(); ?>/assets/img/pttpois.svg" alt="petits pois">
-              <img data-index="step-2" src="// echo get_template_directory_uri(); ?>/assets/img/framboise.svg" alt="framboise">
-              <img data-index="step-3" src=" //echo get_template_directory_uri(); ?>/assets/img/kiwi.svg" alt="kiwi">
-              <img data-index="step-4" src=" //echo get_template_directory_uri(); ?>/assets/img/poire.svg" alt="poire">
-              <img data-index="step-5" src=" //echo get_template_directory_uri(); ?>/assets/img/artichaud.svg" alt="artichaud">
-              <img data-index="step-6" src=" //echo get_template_directory_uri(); ?>/assets/img/orange.svg" alt="orange">
-              <img data-index="step-7" src=" //echo get_template_directory_uri(); ?>/assets/img/chouxfleur.svg" alt="choux fleur">
-              <img data-index="step-8" src=" //echo get_template_directory_uri(); ?>/assets/img/coco.svg" alt="noix de coco">
-              <img data-index="step-9" src=" //echo get_template_directory_uri(); ?>/assets/img/ananas.svg" alt="Ananas">
-              <img data-index="step-10" src=" //echo get_template_directory_uri(); ?>/assets/img/citrouille.svg" alt="citrouille">
-              
-            </div>
-          </div>
-          <div class="col-md-7">
-            <div class="text js-text">
-
-                    
-              <article class="active" data-index="step-1">
-                <h3>Petits pois</h3>
-                <p>
-                  4 semaines : petit pois
-                  Le blastocyste s'implante dans l'utérus.
-                  Poids : Environ 0,004 gramme (4 milligrammes).
-                  Taille : Environ 0,1 mm.
-                </p>
-              </article>
-              <article data-index="step-2">
-                <h3>Framboise</h3>
-                <p>
-                  8 semaines : framboise 
-                  On considère généralement que la grossesse commence à partir de la semaine de retard des règles.
-                  Poids : Environ 1 gramme.
-                  Taille : Environ 1,6 cm.
-                </p>
-              </article>
-              <article data-index="step-3">
-                <h3>Kiwi</h3>
-                <p>
-                  12 semaines : kiwi
-                  Fin du premier trimestre.
-                  Poids : Environ 14 grammes.
-                  Taille : Environ 6,3 cm.
-                </p>
-              </article>
-              <article data-index="step-4">
-                <h3>Poire</h3>
-                <p>
-                  16 semaines : poire
-                  Poids : Environ 100 à 150 grammes.
-                  Taille : Environ 11 à 12,7 cm
-                  Le fœtus a généralement développé des traits faciaux distincts, des doigts et des orteils, et il commence à bouger activement.
-                </p>
-              </article>
-              <article data-index="step-5">
-                <h3>artichaud</h3>
-                <p>
-                  20 semaines : artichaud
-                  Mi-grossesse.
-                  Poids : Environ 300 grammes.
-                  Taille : Environ 16 cm.
-                </p>
-              </article>
-              <article data-index="step-6">
-                <h3>Orange</h3>
-                <p>
-                  24 semaines : orange
-                  Poids : Environ 600 à 700 grammes.
-                  Taille : Environ 30 cm.
-                  Les organes vitaux continuent de se développer, les poumons atteignent un stade où la survie en dehors de l'utérus est envisageable, bien que la prématurité reste un défi important à ce stade.
-                </p>
-              </article>
-              <article data-index="step-7">
-                <h3>Choux-fleur</h3>
-                <p>
-                  28 semaines : choux fleur 
-                  Début du troisième trimestre.
-                  Poids : Environ 1 kg.
-                  Taille : Environ 35,6 cm.
-                </p>
-              </article>
-              <article data-index="step-8">
-                <h3>Noix de coco</h3>
-                <p>
-                  32 semaines : noix de coco
-                  le fœtus se prépare aux fonctions respiratoires et continue de prendre du poids
-                  Poids : Environ 1,8 kg.
-                  Taille : Environ 42 cm.
-                </p>
-              </article>
-              <article data-index="step-9">
-                <h3>Ananas</h3>
-                <p>
-                  36 semaines : ananas
-                  il adopte généralement une position en vue de l'accouchement
-                  Poids : Environ 2,6 kg.
-                  Taille : Environ 47 cm.
-                </p>
-              </article>
-              <article data-index="step-10">
-                <h3>Citrouille</h3>
-                <p>
-                  40 semaines (à terme) : citrouille 
-                  il est prêt pour l'accouchement, démontrant des réflexes développés et un système immunitaire renforcé.
-                  Poids moyen à la naissance : Environ 3,4 kg.
-                  Taille moyenne à la naissance : Environ 50 cm.
-                </p>
-              </article>
-              </div>
-            <ul class="flex-column nav nav-pills links js-links">
-              <li class="nav-item"><a class="nav-link active" data-index="step-1" href="#">4 sem.</a></li>
-              <li class="nav-item"><a class="nav-link" data-index="step-2" href="#">8 sem.</a></li>
-              <li class="nav-item"><a class="nav-link" data-index="step-3" href="#">12 sem.</a></li>
-              <li class="nav-item"><a class="nav-link" data-index="step-4" href="#">16 sem.</a></li>
-              <li class="nav-item"><a class="nav-link" data-index="step-5" href="#">20 sem.</a></li>
-              <li class="nav-item"><a class="nav-link" data-index="step-6" href="#">24 sem.</a></li>
-              <li class="nav-item"><a class="nav-link" data-index="step-7" href="#">28 sem.</a></li>
-              <li class="nav-item"><a class="nav-link" data-index="step-8" href="#">32 sem.</a></li>
-              <li class="nav-item"><a class="nav-link" data-index="step-9" href="#">36 sem.</a></li>
-              <li class="nav-item"><a class="nav-link" data-index="step-10" href="#">40 sem.</a></li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </section> -->
-
+  </section>
+  
       <section>
         <div class="container text-center">
           <div class="row">
