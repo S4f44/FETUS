8647bcf6cd12556d38f4237c2e8a710088d42575
S4f44 <bencersafa8@gmail.com>
2023-12-20 16:48:33 +0100

    réglages probleme safa

--- page-monsuivi.php
+++ page-monsuivi.php
@@ -29,13 +29,12 @@ if (!is_user_logged_in()) { // je vérifie si je suis connecté
 }
 
 $user = wp_get_current_user();
-var_dump($user);
+// var_dump($user);
 
 get_header(); // j'importe mon header
 // attention c'est important de faire les redirection avant le header sinon la redirection ne marche pas
 ?>
 
-coucou c'est une page privée
 
 <?php 
     //$user = wp_get_current_user();
@@ -57,11 +56,7 @@ coucou c'est une page privée
   
   
 
-get_header(); // J'importe mon header
 
-
-// wp_login_form();
-?>
 <section class="presentationttsav">
             <div class="container ">
                 <div class="row">
@@ -186,25 +181,6 @@ get_header(); // J'importe mon header
 
 
 <?php
-// Récupérez les données de la session
-// $donnees_formulaire = isset($_SESSION['donnees_formulaire']) ? $_SESSION['donnees_formulaire'] : '';
-
-// Affichez le contenu en fonction des données du formulaire
-// if (strpos($donnees_formulaire, 'futur_maman_trimestre1') !== false) {
-//     echo '<p>Contenu spécifique pour Futur Maman au Premier trimestre</p>';
-// } elseif (strpos($donnees_formulaire, 'futur_maman_trimestre2') !== false) {
-//     echo '<p>Contenu spécifique pour Futur Maman au Deuxième trimestre</p>';
-// } elseif (strpos($donnees_formulaire, 'futur_maman_trimestre3') !== false) {
-//     echo '<p>Contenu spécifique pour Futur Maman au Troisième trimestre</p>';
-// } elseif (strpos($donnees_formulaire, 'futur_papa_trimestre1') !== false) {
-//     echo '<p>Contenu spécifique pour Futur Papa au Premier trimestre</p>';
-// } elseif (strpos($donnees_formulaire, 'futur_papa_trimestre2') !== false) {
-//     echo '<p>Contenu spécifique pour Futur Papa au Deuxième trimestre</p>';
-// } elseif (strpos($donnees_formulaire, 'futur_papa_trimestre3') !== false) {
-//     echo '<p>Contenu spécifique pour Futur Papa au Troisième trimestre</p>';
-// } else {
-//     echo '<p>Contenu par défaut</p>';
-// }
 
 
 
