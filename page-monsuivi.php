06eb9874d82a033c1113393926fd721c1da96ae1
Nell Gaspar <nell.gaspar09@gmail.com>
2023-12-20 16:15:19 +0100

    -

--- page-monsuivi.php
+++ page-monsuivi.php
@@ -100,8 +100,10 @@ get_header(); // j'importe mon header
 
         <section class="fruits">
                 <div class="container text-center py-3">
-                <div class="row">
-                    <h2>Actuellement, votre bébé fait la taille de ...</h2>
+            
+                    <h2>Actuellement, votre bébé fait la taille de ...</h2>        
+                <div class="my-row row">
+
                     <div class="col-md-6 offset-md-1">
                         <div class="text js-text">
 
